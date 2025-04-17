2268746533c01f8b976f9ec435b112edba1c8284
Raphael Olimpio <murdok.rapha@gmail.com>
2025-04-17 14:16:02 +0000

    altere para mim esse arquivo

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  compiler: {
+    allowedDevOrigins: [
+      '3006-idx-studio-1744895138810.cluster-iesosxm5fzdewqvhlwn5qivgry.cloudworkstations.dev'
+    ],
+  },
 };
 
 export default nextConfig;
